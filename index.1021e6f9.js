f0927469f40a4a1d70d36c93bfcbc1e39e89c427
iuliia-sokol <110597796+iuliia-sokol@users.noreply.github.com>
2022-12-21 16:37:11 +0000

    Deploying to gh-pages from @ iuliia-sokol/Christmas-greeting-PARALLAX@2c133355db650ed0f0c60399a8adc9a45946e979 🚀

---
+++ index.1021e6f9.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(function(){document.documentElement.classList.add("loaded")}));
+//# sourceMappingURL=index.1021e6f9.js.map
---
+++ index.ab3fac70.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(function(){document.documentElement.classList.add("loaded")}));
+//# sourceMappingURL=index.ab3fac70.js.map
